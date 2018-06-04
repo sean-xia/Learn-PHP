a521872e9de5dd36b4c3e77ffd26980d7a0cde07
xhx <xiahongxing@gmail.com>
2018-06-04 15:34:58 +0800

    add cahpter3

--- chapt2.php
+++ chapt2.php
@@ -27,4 +27,41 @@ function example($num)
     echo "$num*$num =".$num * $num;
 }
 example(3);
+
+echo "<br>";
+
+echo "比较函数参数按值传递和按引用传递差别"."<br>";
+
+function test(&$p1, $p2, $p3){
+    echo '$p1 is:'.$p1."<br>";
+    $p1++;
+    echo '$p2 is:'.$p2."<br>";
+    echo '$p3 is:'.$p3."<br>";
+}
+
+$number1 = 1;
+$number2 = 2;
+$number3 = 3;
+
+test($number1,$number2, $number3);
+
+echo "<br>";
+echo '$number1 is:'.$number1."<br>";
+echo '$number2 is:'.$number2."<br>";
+echo '$number3 is:'.$number3."<br>";
+
+echo (int)((0.4 + 0.1)*10);
+
+function calc_rmb($yuan, $jiao, $fen){
+    echo "<br>";
+    echo "人民币总额为：".$yuan."元".$jiao."角".$fen."分"."<br>";
+
+}
+echo "第三套人民币面额为：";
+calc_rmb(18,8,8);
+
+echo "第五套人民币面额为：";
+calc_rmb(188,8,8);
+
+
 ?>
---
+++ chapt3.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xhx
+ * Date: 2018/6/4
+ * Time: 15:30
+ */
+echo "Codes in Chapter3<br>";
+
