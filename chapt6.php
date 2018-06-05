27944f479bd0b88710c6096b527b992c9d93a7b5
xhx <xhx@xhxdeAir.lan>
2018-06-05 20:55:32 +0800

    chpt6

---
+++ chapt6.php
@@ -0,0 +1,90 @@
+<?php
+$data = array(
+	array('id'=>1, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>2, 'title'=>'法国综合性大学怎么样'),
+	array('id'=>3, 'title'=>'香港研究生留学申请时间表'),
+	array('id'=>4, 'title'=>'英国留学找工作'),
+	array('id'=>5, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>6, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>7, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>8, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>9, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>10, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>11, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>12, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>13, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>14, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>15, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>16, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>17, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>18, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>19, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>20, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>21, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>22, 'title'=>'西班牙留学面签常见问答'),
+	array('id'=>23, 'title'=>'西班牙留学面签常见问答')
+);
+
+$totalItems = count($data);
+$itemsPerPage = 5;	//每页显示条数
+$currentPage = isset($_GET['Page'])?$_GET['page']:1;  //获取当前页码
+$urlPattern = 'index.php?page=(:num)';   //拼接url
+
+//实例化分页类
+$pageinator = new Pageinator($totalItems, $itemsPerPage, $currentPage, $urlPattern);
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+>
